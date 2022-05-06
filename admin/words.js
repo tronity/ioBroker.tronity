d139285e13bdb9477234241722eb273ce2ae848c
Sebastian <sebastian@tronity.io>
2022-05-06 18:59:00 +0200

    init

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+	'tronity adapter settings': {
+		en: 'Adapter settings for tronity',
+		de: 'Adaptereinstellungen für tronity',
+		ru: 'Настройки адаптера для tronity',
+		pt: 'Configurações do adaptador para tronity',
+		nl: 'Adapterinstellingen voor tronity',
+		fr: "Paramètres d'adaptateur pour tronity",
+		it: "Impostazioni dell'adattatore per tronity",
+		es: 'Ajustes del adaptador para tronity',
+		pl: 'Ustawienia adaptera dla tronity',
+		'zh-cn': 'tronity的适配器设置',
+	},
+	option1: {
+		en: 'option1',
+		de: 'Option 1',
+		ru: 'Опция 1',
+		pt: 'Opção 1',
+		nl: 'Optie 1',
+		fr: 'Option 1',
+		it: 'opzione 1',
+		es: 'Opción 1',
+		pl: 'opcja 1',
+		'zh-cn': '选项1',
+	},
+	option2: {
+		en: 'option2',
+		de: 'Option 2',
+		ru: 'вариант 2',
+		pt: 'opção 2',
+		nl: 'Optie 2',
+		fr: 'Option 2',
+		it: 'opzione 2',
+		es: 'opcion 2',
+		pl: 'Opcja 2',
+		'zh-cn': '选项2',
+	},
+};
