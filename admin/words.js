3ecdb6049e2bec92845ae66d193a3ab4915ae9a4
Sebastian <sebastian@tronity.io>
2022-05-09 18:28:49 +0200

    fix errors

--- admin/words.js
+++ admin/words.js
@@ -2,20 +2,52 @@
 'use strict';
 
 systemDictionary = {
-	'Adapter settings for Tronity': {
-		'en': 'Adapter settings for Tronity',
-		'de': 'Adaptereinstellungen für Tronity',
+	"Adapter settings for Tronity": {
+		"en": "Adapter settings for TRONITY",
+		"de": "Adaptereinstellungen für TRONITY",
+		"ru": "Настройки адаптера для Тронити",
+		"pt": "Configurações do adaptador para TRONITY",
+		"nl": "Adapterinstellingen voor TRONITY",
+		"fr": "Paramètres de l'adaptateur pour TRONITY",
+		"it": "Impostazioni dell'adattatore per TRONITY",
+		"es": "Configuración del adaptador para TRONITY",
+		"pl": "Ustawienia adaptera dla TRONITY",
+		"zh-cn": "TRONITY 的适配器设置"
 	},
-	'Description': {
-		'en': 'To retrieve data, client_id and client_secret must be entered from the Tronity Platform. Then check the values and select the vehicle.',
-		'de': 'Um Daten abzurufen müssen client_id und client_secret von der Tronity Platform eingegeben werden. Danach die Werte prüfen und das Fahrzeug auswählen.'
+	"Description": {
+		"en": "To retrieve data, client_id and client_secret must be entered from the TRONITY Platform. Then check the values and select the vehicle.",
+		"de": "Um Daten abzurufen, müssen client_id und client_secret von der TRONITY-Plattform eingegeben werden. Überprüfen Sie dann die Werte und wählen Sie das Fahrzeug aus.",
+		"ru": "Чтобы получить данные, client_id и client_secret должны быть введены с платформы TRONITY. Затем проверьте значения и выберите автомобиль.",
+		"pt": "Para recuperar dados, client_id e client_secret devem ser inseridos na plataforma TRONITY. Em seguida, verifique os valores e selecione o veículo.",
+		"nl": "Om gegevens op te halen, moeten client_id en client_secret worden ingevoerd vanaf het TRONITY-platform. Controleer vervolgens de waarden en selecteer het voertuig.",
+		"fr": "Pour récupérer des données, client_id et client_secret doivent être saisis à partir de la plate-forme TRONITY. Vérifiez ensuite les valeurs et sélectionnez le véhicule.",
+		"it": "Per recuperare i dati, è necessario inserire client_id e client_secret dalla piattaforma TRONITY. Quindi controlla i valori e seleziona il veicolo.",
+		"es": "Para recuperar datos, se deben ingresar client_id y client_secret desde la plataforma TRONITY. Luego verifique los valores y seleccione el vehículo.",
+		"pl": "Aby pobrać dane, client_id i client_secret muszą być wprowadzone z Platformy TRONITY. Następnie sprawdź wartości i wybierz pojazd.",
+		"zh-cn": "要检索数据，必须从 TRONITY 平台输入 client_id 和 client_secret。然后检查值并选择车辆。"
 	},
-	'Check Login Data': {
-		'en': 'Check Login Data',
-		'de': 'Login-Daten prüfen'
+	"Check Login Data": {
+		"en": "Check Login Data",
+		"de": "Anmeldedaten prüfen",
+		"ru": "Проверить данные для входа",
+		"pt": "Verificar dados de login",
+		"nl": "Inloggegevens controleren",
+		"fr": "Vérifier les données de connexion",
+		"it": "Controlla i dati di accesso",
+		"es": "Comprobar datos de inicio de sesión",
+		"pl": "Sprawdź dane logowania",
+		"zh-cn": "检查登录数据"
 	},
-	'Select your Car': {
-		'en': 'Select your Car',
-		'de': 'Wählen Sie Ihr Fahrzeug aus'
+	"Select your Car": {
+		"en": "Select your Car",
+		"de": "Wählen Sie Ihr Auto aus",
+		"ru": "Выберите свой автомобиль",
+		"pt": "Selecione seu carro",
+		"nl": "Selecteer uw auto",
+		"fr": "Sélectionnez votre voiture",
+		"it": "Seleziona la tua auto",
+		"es": "Seleccione su coche",
+		"pl": "Wybierz swój samochód",
+		"zh-cn": "选择您的汽车"
 	}
 };
