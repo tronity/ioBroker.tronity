0b60ccd42bbd53889111e1579a59d1aad998278d
stevenengel-cmyk <steven.engel@tronity.com>
2026-06-10 15:47:00 +0200

    chore: add integration controller version

--- test/integration.js
+++ test/integration.js
@@ -2,4 +2,5 @@ const path = require('path');
 const { tests } = require('@iobroker/testing');
 
 // Run integration tests - See https://github.com/ioBroker/testing for a detailed explanation and further options
-tests.integration(path.join(__dirname, '..'));
+const controllerVersion = process.env.JS_CONTROLLER_VERSION || '6.0.11';
+tests.integration(path.join(__dirname, '..'), { controllerVersion });
